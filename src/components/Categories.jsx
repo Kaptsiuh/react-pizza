c532f2c7c0eeb48a6bb16fac9ad1d9982a33bb4d
Dima <dimaberry1@gmail.com>
2023-08-03 23:52:31 +0200

    feat: add category switching

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,21 @@
+import React from 'react';
+
 const Categories = () => {
+  const [activeCategory, setActiveCategory] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Гриль', 'Острые', 'Закрытые']
+
+  const onClickCategory = (index) => {
+    setActiveCategory(index);
+  }
+
   return ( 
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+        <li onClick={() => onClickCategory(index)} className={activeCategory === index ? 'active' : ''}>
+          {value}
+        </li>))}
       </ul>
     </div>
    );
