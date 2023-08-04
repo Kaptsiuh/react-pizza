000f54f1666e9186b0a8a9ad623885159f2d4796
Dima <dimaberry1@gmail.com>
2023-08-04 02:06:08 +0200

    feat: add cards with pizzas, swiching sizes and type of dough

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from "./assets/pizzas.json";
+
 import "./scss/app.scss";
 
 function App() {
@@ -20,7 +22,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              <PizzaBlock title={"Diablo"} price={666} />
+              {pizzas.map((obj) => (
+                <PizzaBlock {...obj} />
+              ))}
             </div>
           </div>
         </div>
