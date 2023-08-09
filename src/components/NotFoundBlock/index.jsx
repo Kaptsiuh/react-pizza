ff2bde82cb5e1c6a5e55ef00cac5316e35a33049
Dima <dimaberry1@gmail.com>
2023-08-09 23:28:12 +0200

    feat: add cart pege, add responsive desing

--- src/components/NotFoundBlock/index.jsx
+++ src/components/NotFoundBlock/index.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import styles from './NotFoundBlock.module.scss';
 
 const NotFoundBlock = () => {
-  return ( 
-    <div className={styles.root}>
+  return (
+    <div className="container">
+      <div className={styles.root}>
       <h1> 
         <span>&#128543;</span>
         <br />
         Not Found 
       </h1>
-      <p></p>
     </div>
+    </div> 
    );
 }
  
