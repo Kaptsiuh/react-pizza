0f6012768f1db1ed028c5da96cec741eaeaa094d
Dima <dimaberry1@gmail.com>
2023-08-16 18:10:46 +0200

    refactor: changed search, use debounce and useCallback

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 
 const Search = () => {
+  const [value, setValue] = React.useState('');
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    setValue('');
+    inputRef.current.focus();
+  }
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000), 
+    []
+  );
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+    updateSearchValue(e.target.value);
+  }
 
   return ( 
     <div className={styles.root}>
@@ -20,13 +41,14 @@ const Search = () => {
         <line fill="none" stroke="#000000" strokeMiterlimit="10" strokeWidth="4" x1="32.229" x2="45.5" y1="32.229" y2="45.5"/>
       </svg>
       <input 
-      value={searchValue}
-      onChange={event => setSearchValue(event.target.value)} 
+      ref={inputRef}
+      value={value}
+      onChange={onChangeInput} 
       className={styles.input} 
       placeholder='Search pizzas...' />
         {searchValue && (
         <svg 
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           className={styles.close}
           viewBox="0 0 32 32" 
           xmlns="http://www.w3.org/2000/svg">
