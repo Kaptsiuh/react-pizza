0e553ea19174795f9719f523e029433be9aa56f2
Dima <dimaberry1@gmail.com>
2023-08-29 21:01:09 +0200

    feat: CartItem, CartEmpty, Categories, Header, Sort migrate to typescrip

---
+++ src/components/CartEmpty.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import cartEmptyImg from "../assets/img/empty-cart.png";
+
+const CartEmpty: React.FC = () => {
+  return (
+    <>
+      <div className="cart cart--empty">
+        <h2>Cart Empty</h2>
+        <p>
+          You might not have ordered pizza?
+          <br />
+          To order pizza, please go to the main page.
+        </p>
+        <img src={cartEmptyImg} alt="Empty cart" />
+        <Link to="/" className="button button--black">
+          <span>Go back</span>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default CartEmpty;
